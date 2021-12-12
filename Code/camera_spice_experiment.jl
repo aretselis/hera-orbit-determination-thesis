c1a9147609bc9f0b8887360edf337e59686847e3
retse <anast.rets@hotmail.com>
2021-12-12 21:27:23 +0200

    Correctly rotate HERA_AFC and generate first images by plotting on a plane

--- Code/camera_spice_experiment.jl
+++ Code/camera_spice_experiment.jl
@@ -1,7 +1,45 @@
-using SPICE, ProgressBars, Plots
-# using FileIO, GLMakie
+using SPICE, ProgressBars, Plots, LinearAlgebra
 include("orbital_utilities.jl")
-include("Propagators.jl")
+include("propagators.jl")
+include("spice_utilities.jl")
+include("camera_utilities.jl")
+
+
+function load_hera_spice_kernels()
+    # Extract Hera orbit from SPICE
+    # Load leap seconds kernel
+    leap_kernel = "C:\\Users\\retse\\repos\\hera-data\\kernels\\lsk\\naif0012.tls"
+    furnsh(leap_kernel)
+    # Load all planets, Didymos-Dimorphos and HERA kernels
+    planets_kernel = "C:\\Users\\retse\\repos\\hera-data\\kernels\\spk\\de432s.bsp"
+    didymos_kernel = "C:\\Users\\retse\\repos\\hera-data\\kernels\\spk\\HERA_didymain_DCP3_v01.bsp"
+    dimorphos_kernel = "C:\\Users\\retse\\repos\\hera-data\\kernels\\spk\\HERA_didymoon_DCP3_v01.bsp"
+    hera_kernel = "C:\\Users\\retse\\repos\\hera-data\\kernels\\spk\\HERA_sc_PO_v01.bsp"
+    hera_ck = "C:\\Users\\retse\\repos\\hera-data\\kernels\\ck\\hera_sc_PO_EMA_20270209_20270727_f20181203_v03.bc"
+    hera_equipment = "C:\\Users\\retse\\repos\\hera-data\\kernels\\fk\\hera_v07.tf"
+    hera_pck = "C:\\Users\\retse\\repos\\hera-data\\kernels\\pck\\hera_didymos_v04.tpc"
+    bodies_pck = "C:\\Users\\retse\\repos\\hera-data\\kernels\\pck\\de-403-masses.tpc"
+    hera_sclk = "C:\\Users\\retse\\repos\\hera-data\\kernels\\sclk\\hera_fict_20181203.tsc"
+    hera_instruments = "C:\\Users\\retse\\repos\\hera-data\\kernels\\ik\\hera_afc_v03.ti"
+    furnsh("C:\\Users\\retse\\repos\\hera-data\\kernels\\spk\\HERA_didymain_DCP1_v01.bsp")
+    furnsh("C:\\Users\\retse\\repos\\hera-data\\kernels\\spk\\HERA_didymain_DCP3VCF_v01.bsp")
+    furnsh("C:\\Users\\retse\\repos\\hera-data\\kernels\\spk\\HERA_didymain_DCP3_v01.bsp")
+    furnsh("C:\\Users\\retse\\repos\\hera-data\\kernels\\spk\\HERA_didymain_ECP_v01.bsp")
+    furnsh("C:\\Users\\retse\\repos\\hera-data\\kernels\\spk\\HERA_didymoon_DCP1_v01.bsp")
+    furnsh("C:\\Users\\retse\\repos\\hera-data\\kernels\\spk\\HERA_didymoon_DCP3VCF_v01.bsp")
+    furnsh("C:\\Users\\retse\\repos\\hera-data\\kernels\\spk\\HERA_didymoon_DCP3_v01.bsp")
+    furnsh("C:\\Users\\retse\\repos\\hera-data\\kernels\\spk\\HERA_didymoon_ECP_v01.bsp")
+    furnsh(planets_kernel)
+    furnsh(didymos_kernel)
+    furnsh(dimorphos_kernel)
+    furnsh(hera_kernel)
+    furnsh(hera_ck)
+    furnsh(hera_equipment)
+    furnsh(hera_pck)
+    furnsh(bodies_pck)
+    furnsh(hera_sclk)
+    furnsh(hera_instruments)
+end
 
 
 function main()
@@ -39,54 +77,37 @@ function main()
     x_dimorphos, y_dimorphos, z_dimorphos, vx_dimorphos, vy_dimorphos, vz_dimorphos, t_vector = runge_kutta_4(x, y, z, vx, vy, vz, mu_system, start_time, end_time, step_size)
     dimorphos_coordinates = hcat(x_dimorphos/1000, y_dimorphos/1000, z_dimorphos/1000)
 
-    # Extract Hera orbit from SPICE
-    # Load leap seconds kernel
-    leap_kernel = "C:\\Users\\retse\\repos\\hera-data\\kernels\\lsk\\naif0012.tls"
-    furnsh(leap_kernel)
-    # Load all planets, Didymos-Dimorphos and HERA kernels
-    planets_kernel = "C:\\Users\\retse\\repos\\hera-data\\kernels\\spk\\de432s.bsp"
-    didymos_kernel = "C:\\Users\\retse\\repos\\hera-data\\kernels\\spk\\HERA_didymain_DCP3_v01.bsp"
-    dimorphos_kernel = "C:\\Users\\retse\\repos\\hera-data\\kernels\\spk\\HERA_didymoon_DCP3_v01.bsp"
-    hera_kernel = "C:\\Users\\retse\\repos\\hera-data\\kernels\\spk\\HERA_sc_DCP3_v01.bsp"
-    hera_ck = "C:\\Users\\retse\\repos\\hera-data\\kernels\\ck\\hera_sc_PO_EMA_20270209_20270727_f20181203_v03.bc"
-    hera_equipment = "C:\\Users\\retse\\repos\\hera-data\\kernels\\fk\\hera_v06.tf"
-    hera_pck = "C:\\Users\\retse\\repos\\hera-data\\kernels\\pck\\hera_didymos_v03.tpc"
-    bodies_pck = "C:\\Users\\retse\\repos\\hera-data\\kernels\\pck\\de-403-masses.tpc"
-    hera_sclk = "C:\\Users\\retse\\repos\\hera-data\\kernels\\sclk\\hera_fict_20181203.tsc"
-    hera_instruments = "C:\\Users\\retse\\repos\\hera-data\\kernels\\ik\\hera_afc_v03.ti"
-    furnsh(planets_kernel)
-    furnsh(didymos_kernel)
-    furnsh(dimorphos_kernel)
-    furnsh(hera_kernel)
-    furnsh(hera_ck)
-    furnsh(hera_equipment)
-    furnsh(hera_pck)
-    furnsh(bodies_pck)
-    furnsh(hera_sclk)
-    furnsh(hera_instruments)
+    load_hera_spice_kernels()
 
     # Orbit start and end time (for SPICE)
-    spice_start_time = utc2et("2027-04-25T08:14:58")
+    spice_start_time = utc2et("2027-02-25T08:14:58")
     spice_end_time = spice_start_time + end_time
-    println(spice_end_time)
 
     # Initialize position arrays (x, y, z)
     didymos_coordinates = zeros(Float64, number_of_steps + 1, 3)
-    #dimorphos_coordinates = zeros(Float64, number_of_steps, 3)
-    spice_dimorphos_coordinates = zeros(Float64, number_of_steps + 1, 3)
     barycenter_coordinates = zeros(Float64, number_of_steps + 1, 3)
-    barycenter_offset = zeros(Float64, 1, 3)
-    barycenter_initial_coordinates = zeros(Float64, 1, 3)
+    barycenter_offset = zeros(Float64, 3)
+    barycenter_initial_coordinates = zeros(Float64, 3)
+    x_pixel_coordinates_didymos = zeros(Float64, number_of_steps + 1)
+    y_pixel_coordinates_didymos = zeros(Float64, number_of_steps + 1)
+    x_pixel_coordinates_dimorphos = zeros(Float64, number_of_steps + 1)
+    y_pixel_coordinates_dimorphos = zeros(Float64, number_of_steps + 1)
+    camera_position_didymos = zeros(Float64, number_of_steps + 1, 3)
+    camera_position_dimorphos = zeros(Float64, number_of_steps + 1, 3)
 
-    # Main spice calculations loop
+    # HERA_AFC-1 coordinate system unit vectors
+    e_x = [1.0, 0.0, 0.0]
+    e_y = [0.0, 1.0, 0.0]
+    e_z = [0.0, 0.0, 1.0]
+
+    # Main SPICE calculations loop
     iteration = 1
     println("\nSPICE calculations:")
     for i in ProgressBar(spice_start_time:step_size:spice_end_time)
-        position_didymos, lt = spkpos("-658030", i, "HERA_SPACECRAFT", "None", "-999")
-        spice_position_dimorphos, lt = spkpos("-658031", i, "HERA_SPACECRAFT", "None", "-999")
+        position_didymos, lt = spkpos("-658030", i, "HERA_AFC-1", "None", "-999")
+        position_system_barycenter, lt = spkpos("2065803", i, "HERA_AFC-1", "None", "-999")          
         rotation_frame = pxform("J2000", "HERA_SPACECRAFT", i)
         position_dimorphos = rotation_frame * dimorphos_coordinates[iteration, :]
-        position_system_barycenter, lt = spkpos("2065803", i, "HERA_SPACECRAFT", "None", "-999")
         # Get didymos reference position or translate hera_coordinates
         if iteration == 1
             for j in 1:3
@@ -97,43 +118,73 @@ function main()
                 barycenter_offset[j] = barycenter_initial_coordinates[j] - position_system_barycenter[j]
             end
         end
+        # Apply offset to all coordinates
         for j in 1:3
-            didymos_coordinates[iteration, j] = barycenter_offset[j] + position_didymos[j]
-            spice_dimorphos_coordinates[iteration, j] = barycenter_offset[j] + spice_position_dimorphos[j]
-            barycenter_coordinates[iteration, j] = barycenter_offset[j] + position_system_barycenter[j]
+            barycenter_coordinates[iteration, j] =  position_system_barycenter[j] + barycenter_offset[j]
+            didymos_coordinates[iteration, j] =  position_didymos[j] + barycenter_offset[j]
             dimorphos_coordinates[iteration, j] =  position_dimorphos[j] + barycenter_initial_coordinates[j]
         end
+        # Rotate HERA_AFC-1 camera reference frame assuming it is always tracking the barycenter
+        rotation_matrix = compute_rotation_matrix(barycenter_coordinates[iteration, :], e_z)
+        barycenter_coordinates[iteration, :] = rotation_matrix * barycenter_coordinates[iteration, :]
+        didymos_coordinates[iteration, :] =  rotation_matrix * didymos_coordinates[iteration, :]
+        dimorphos_coordinates[iteration, :] =  rotation_matrix * dimorphos_coordinates[iteration, :]
+        # Picture generation
+        rotation_matrix = compute_rotation_matrix(e_z, barycenter_coordinates[iteration, :])
+        camera_position_didymos[iteration, :] = rotation_matrix * didymos_coordinates[iteration, :]
+        camera_position_dimorphos[iteration, :] = rotation_matrix * dimorphos_coordinates[iteration, :]
+        x_pixel_coordinates_didymos[iteration] = camera_position_didymos[iteration, 1]/camera_position_didymos[iteration, 3]
+        y_pixel_coordinates_didymos[iteration] = camera_position_didymos[iteration, 2]/camera_position_didymos[iteration, 3]
+        x_pixel_coordinates_dimorphos[iteration] = camera_position_dimorphos[iteration, 1]/camera_position_dimorphos[iteration, 3]
+        y_pixel_coordinates_dimorphos[iteration] = camera_position_dimorphos[iteration, 2]/camera_position_dimorphos[iteration, 3]
         iteration += 1
     end
+
+    # Compute boundary vectors/points for the camera
+    boresight_vector = barycenter_coordinates[iteration-1, :]
+    rotation_matrix = compute_rotation_matrix(boresight_vector, e_z)
+    camera_id = -999110
+    boundary_vectors_matrix = get_boundary_vectors(camera_id)
+    line_generator = LinRange(0, 30, 10)
+
+    # Rotate camera boundary vectors to assume camera is always tracking barycenter
+    for i in 1:4
+        result = rotation_matrix * boundary_vectors_matrix[i, :]
+        for j in 1:3
+            boundary_vectors_matrix[i,j] = result[j]
+        end
+    end
+
+    # Free used kernels
     kclear()
 
-    # Plot
-    
+    # Plot 3D image for reference    
     x_didymos, y_didymos, z_didymos = didymos_coordinates[:, 1], didymos_coordinates[:, 2], didymos_coordinates[:, 3]
     x_dimorphos, y_dimorphos, z_dimorphos = dimorphos_coordinates[:, 1], dimorphos_coordinates[:, 2], dimorphos_coordinates[:, 3]
-    x_spice_dimorphos, y_spice_dimorphos, z_spice_dimorphos = spice_dimorphos_coordinates[:, 1], spice_dimorphos_coordinates[:, 2], spice_dimorphos_coordinates[:, 3]
     plotlyjs()
-    scatter3d(x_didymos, y_didymos,z_didymos, color = "blue", xlabel="x [km]", ylabel="y [km]", zlabel="z [km]", label="Didymos", markersize = 1)
-    scatter3d!(x_dimorphos, y_dimorphos,z_dimorphos, color = "orange", markersize = 1, label = "Dimorphos")
-    scatter3d!(x_spice_dimorphos, y_spice_dimorphos, z_spice_dimorphos, color = "red", markersize = 1, label = "SPICE Dimorphos")
-    #=
-    # Save image as .gif figure (warning, might take a lot of time)
-    plt = scatter3d(1, title="Hera Trajectory", xaxis=("x",(-20,20)), yaxis=("y",(-20,20)), zaxis=("z",(-25,5)), markersize=1)
-    anim = @animate for i in ProgressBar(1:1:length(x_didymos))
-        push!(plt, x_didymos[i], y_didymos[i], z_didymos[i])
-        push!(plt, x_dimorphos[i], y_dimorphos[i], z_dimorphos[i])
+    plt3d = scatter3d(x_didymos, y_didymos, z_didymos, color = "blue", xlabel="x [km]", ylabel="y [km]", zlabel = "z [km]", label="Didymos", markersize = 1)
+    scatter3d!(x_dimorphos, y_dimorphos, z_dimorphos, color = "orange", markersize = 1, label = "Dimorphos")
+
+    # Plot camera boundary lines
+    sensor_boundary_points = zeros(Float64, 4, 3)
+    x_pixel_coordinates_boundaries = zeros(Float64, 4)
+    y_pixel_coordinates_boundaries = zeros(Float64, 4)
+    for i in 1:4
+        x_line, y_line, z_line = boundary_vectors_matrix[i, 1] * line_generator, boundary_vectors_matrix[i, 2] * line_generator, boundary_vectors_matrix[i, 3] * line_generator
+        sensor_boundary_points[i, 1] = x_line[length(x_line)]
+        sensor_boundary_points[i, 2] = y_line[length(y_line)]
+        sensor_boundary_points[i, 3] = z_line[length(z_line)]
+        x_pixel_coordinates_boundaries[i] = x_line[length(x_line)]/z_line[length(z_line)]
+        y_pixel_coordinates_boundaries[i] = y_line[length(y_line)]/z_line[length(z_line)]
+        camera_label = "Camera boundary line " * string(i)
+        plot3d!(x_line, y_line, z_line, color="green", label=camera_label) 
     end
-    gif(anim, "hera_orbit_new_rf.gif", fps=15)
-    
-    s = Scene()
-    didymos = load("C:\\Users\\retse\\repos\\hera-data\\kernels\\dsk\\g_50677mm_rad_obj_dida_0000n00000_v001.obj")
-    dimorphos = load("C:\\Users\\retse\\repos\\hera-data\\kernels\\dsk\\g_08438mm_lgt_obj_didb_0000n00000_v002.obj")
-    mesh!(s, didymos, color = "gray")
-    #translate!(s, 2.0)
-    mesh!(s, dimorphos, color = "blue")
-    s
-    =#
- 
+
+    # Plot 2D image simulation   
+    plt2d = scatter(x_pixel_coordinates_didymos, y_pixel_coordinates_didymos, label= "Didymos")
+    scatter!(x_pixel_coordinates_dimorphos, y_pixel_coordinates_dimorphos, label = "Dimorphos")
+    scatter!(x_pixel_coordinates_boundaries, y_pixel_coordinates_boundaries, label = "Camera boundaries")
+    display(plt3d)
 end
 
 main()
