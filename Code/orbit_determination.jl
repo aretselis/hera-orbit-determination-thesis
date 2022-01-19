d06a47a173bc65b4a44823237be0da8f1828672f
retse <anast.rets@hotmail.com>
2022-01-19 13:58:13 +0200

    Plot spatial resolution vs time

--- Code/orbit_determination.jl
+++ Code/orbit_determination.jl
@@ -1,6 +1,7 @@
 using SPICE, ProgressBars, Plots
 include("orbital_utilities.jl")
 include("Propagators.jl")
+include("errors.jl")
 
 function main()
     # System properties
@@ -45,18 +46,39 @@ function main()
     planets_kernel = "C:\\Users\\retse\\repos\\hera-data\\kernels\\spk\\de432s.bsp"
     didymos_kernel = "C:\\Users\\retse\\repos\\hera-data\\kernels\\spk\\HERA_didymain_DCP3_v01.bsp"
     dimorphos_kernel = "C:\\Users\\retse\\repos\\hera-data\\kernels\\spk\\HERA_didymoon_DCP3_v01.bsp"
-    hera_kernel = "C:\\Users\\retse\\repos\\hera-data\\kernels\\spk\\HERA_sc_DCP3_v01.bsp"
+    hera_kernel = "C:\\Users\\retse\\repos\\hera-data\\kernels\\spk\\HERA_sc_PO_v01.bsp"
+    hera_ck = "C:\\Users\\retse\\repos\\hera-data\\kernels\\ck\\hera_sc_PO_EMA_20270209_20270727_f20181203_v03.bc"
+    hera_equipment = "C:\\Users\\retse\\repos\\hera-data\\kernels\\fk\\hera_v07.tf"
+    hera_pck = "C:\\Users\\retse\\repos\\hera-data\\kernels\\pck\\hera_didymos_v04.tpc"
+    bodies_pck = "C:\\Users\\retse\\repos\\hera-data\\kernels\\pck\\de-403-masses.tpc"
+    hera_sclk = "C:\\Users\\retse\\repos\\hera-data\\kernels\\sclk\\hera_fict_20181203.tsc"
+    hera_instruments = "C:\\Users\\retse\\repos\\hera-data\\kernels\\ik\\hera_afc_v03.ti"
+    furnsh("C:\\Users\\retse\\repos\\hera-data\\kernels\\spk\\HERA_didymain_DCP1_v01.bsp")
+    furnsh("C:\\Users\\retse\\repos\\hera-data\\kernels\\spk\\HERA_didymain_DCP3VCF_v01.bsp")
+    furnsh("C:\\Users\\retse\\repos\\hera-data\\kernels\\spk\\HERA_didymain_DCP3_v01.bsp")
+    furnsh("C:\\Users\\retse\\repos\\hera-data\\kernels\\spk\\HERA_didymain_ECP_v01.bsp")
+    furnsh("C:\\Users\\retse\\repos\\hera-data\\kernels\\spk\\HERA_didymoon_DCP1_v01.bsp")
+    furnsh("C:\\Users\\retse\\repos\\hera-data\\kernels\\spk\\HERA_didymoon_DCP3VCF_v01.bsp")
+    furnsh("C:\\Users\\retse\\repos\\hera-data\\kernels\\spk\\HERA_didymoon_DCP3_v01.bsp")
+    furnsh("C:\\Users\\retse\\repos\\hera-data\\kernels\\spk\\HERA_didymoon_ECP_v01.bsp")
     furnsh(planets_kernel)
     furnsh(didymos_kernel)
     furnsh(dimorphos_kernel)
     furnsh(hera_kernel)
+    furnsh(hera_ck)
+    furnsh(hera_equipment)
+    furnsh(hera_pck)
+    furnsh(bodies_pck)
+    furnsh(hera_sclk)
+    furnsh(hera_instruments)
 
     # Orbit start and end time
-    spice_start_time = utc2et("2027-04-25T08:14:58")
+    spice_start_time = utc2et("2027-02-25T08:14:58")
     spice_end_time = spice_start_time + end_time
 
     # Initialize position arrays (x, y, z)
     hera_coordinates = zeros(Float64, number_of_steps, 3)
+    spatial_resolution = zeros(Float64, number_of_steps)
 
     # Main spice calculations loop
     iteration = 1
@@ -66,6 +88,8 @@ function main()
         for j in 1:3
             hera_coordinates[iteration, j] = hera_position[j]
         end
+        distance = sqrt(hera_coordinates[iteration, 1]^2+hera_coordinates[iteration, 2]^2+hera_coordinates[iteration, 3]^2)
+        spatial_resolution[iteration] = spatial_resolution_calculator(distance)
         iteration += 1
     end
 
@@ -75,8 +99,11 @@ function main()
     # Plot Results
     
     plotlyjs()
-    plot3d(x_dimorphos/1000,y_dimorphos/1000,z_dimorphos/1000,title="System Trajectories", xlabel="x [km]", ylabel="y [km]", zlabel="z [km]", label="Dimorphos")
+    plt_3d = plot3d(x_dimorphos/1000,y_dimorphos/1000,z_dimorphos/1000,title="System Trajectories", xlabel="x [km]", ylabel="y [km]", zlabel="z [km]", label="Dimorphos")
     plot3d!(x_hera,y_hera,z_hera, xlabel="x [km]", ylabel="y [km]", zlabel="z [km]", label="Hera")
+
+    plt_distance = plot(t_vector[1:10000], spatial_resolution, label="Spatial resolution", xlabel="Time", ylabel="Spatial resolution, [m]", title="Spatial resolution vs Time")
+    display(plt_distance)
 end
 
 main()
