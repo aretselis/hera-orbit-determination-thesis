e6b62099847f127e233b1d54ae76e96406d8c62b
retse <anast.rets@hotmail.com>
2022-02-20 23:53:46 +0200

    Use fit mask for specific elements

--- Code/optimization.jl
+++ Code/optimization.jl
@@ -1,13 +1,21 @@
 function residuals(orbit)
+    # Check for elements to be fitted
+    for i in 1:6
+        if fit_mask[i] == false
+            orbit[i] = initial_guess[i]
+        end
+    end
+    # Select elements
     a = orbit[1]
     e = orbit[2]
     i = orbit[3]
     Omega = orbit[4]
     omega = orbit[5]
     M = orbit[6]
-    observed_x, oberved_y = x_pixel_dimorphos, y_pixel_dimorphos
+    # Compute dimorphos pixel points for given orbit and compute xhi2
+    observed_x, observed_y = x_pixel_dimorphos, y_pixel_dimorphos
     predicted_x, predicted_y = propagate_and_compute_dimorphos_pixel_points(a, e, i, Omega, omega, M, start_time, end_time, step_size, spice_start_time)
-    xhi2 = sum_of_squared_residuals(observed_x, oberved_y, predicted_x, predicted_y)
+    xhi2 = sum_of_squared_residuals(observed_x, observed_y, predicted_x, predicted_y)
     return xhi2
 end
 
