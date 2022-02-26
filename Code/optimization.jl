62921390d222392ff235aee4134fdae31f5a375b
retse <anast.rets@hotmail.com>
2022-02-26 23:57:33 +0200

    Correctly deal with pictures missing Didymos or Dimorphos

--- Code/optimization.jl
+++ Code/optimization.jl
@@ -20,14 +20,16 @@ function residuals(orbit)
     observed_x, observed_y = x_pixel_dimorphos, y_pixel_dimorphos
     predicted_x, predicted_y = propagate_and_compute_dimorphos_pixel_points(a, e, i, Omega, omega, M, start_time, end_time, step_size, spice_start_time)
     xhi2 = sum_of_squared_residuals(observed_x, observed_y, predicted_x, predicted_y)
-    return xhi2
+    return Float64(xhi2)
 end
 
 
 function sum_of_squared_residuals(observed_x, observed_y, predicted_x, predicted_y)
-    typeof(predicted_y)
     x_residuals = observed_x - predicted_x
     y_residuals = observed_y - predicted_y
+    # Clean up missing values
+    x_residuals = collect(skipmissing(x_residuals))
+    y_residuals = collect(skipmissing(y_residuals))
     squared_sum = sum(x_residuals.^2) + sum(y_residuals.^2)
     return squared_sum
 end
