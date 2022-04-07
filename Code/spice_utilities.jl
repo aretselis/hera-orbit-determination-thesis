cce06d2ba23e160da902090e343526c28c23245a
retse <anast.rets@hotmail.com>
2022-04-07 19:05:05 +0300

    Update required SPICE kernels

--- Code/spice_utilities.jl
+++ Code/spice_utilities.jl
@@ -7,10 +7,8 @@ function load_hera_spice_kernels()
 
     NOTE: Make sure to clear the kernels after you are done by using kclear()
     =#
-    # Extract Hera orbit from SPICE
     # Load leap seconds kernel
     leap_kernel = "C:\\Users\\retse\\repos\\hera-data\\kernels\\lsk\\naif0012.tls"
-    furnsh(leap_kernel)
     # Load all planets, Didymos-Dimorphos and HERA kernels
     planets_kernel = "C:\\Users\\retse\\repos\\hera-data\\kernels\\spk\\de432s.bsp"
     didymos_kernel = "C:\\Users\\retse\\repos\\hera-data\\kernels\\spk\\HERA_didymain_DCP3_v01.bsp"
@@ -23,14 +21,13 @@ function load_hera_spice_kernels()
     bodies_pck = "C:\\Users\\retse\\repos\\hera-data\\kernels\\pck\\de-403-masses.tpc"
     hera_sclk = "C:\\Users\\retse\\repos\\hera-data\\kernels\\sclk\\hera_fict_20181203.tsc"
     hera_instruments = "C:\\Users\\retse\\repos\\hera-data\\kernels\\ik\\hera_afc_v03.ti"
-    furnsh("C:\\Users\\retse\\repos\\hera-data\\kernels\\spk\\HERA_didymain_DCP1_v01.bsp")
-    furnsh("C:\\Users\\retse\\repos\\hera-data\\kernels\\spk\\HERA_didymain_DCP3VCF_v01.bsp")
-    furnsh("C:\\Users\\retse\\repos\\hera-data\\kernels\\spk\\HERA_didymain_DCP3_v01.bsp")
-    furnsh("C:\\Users\\retse\\repos\\hera-data\\kernels\\spk\\HERA_didymain_ECP_v01.bsp")
-    furnsh("C:\\Users\\retse\\repos\\hera-data\\kernels\\spk\\HERA_didymoon_DCP1_v01.bsp")
-    furnsh("C:\\Users\\retse\\repos\\hera-data\\kernels\\spk\\HERA_didymoon_DCP3VCF_v01.bsp")
-    furnsh("C:\\Users\\retse\\repos\\hera-data\\kernels\\spk\\HERA_didymoon_DCP3_v01.bsp")
-    furnsh("C:\\Users\\retse\\repos\\hera-data\\kernels\\spk\\HERA_didymoon_ECP_v01.bsp")
+    ECP_didymos = "C:\\Users\\retse\\repos\\hera-data\\kernels\\spk\\HERA_didymain_ECP_v01.bsp"
+    ECP_dimorpos = "C:\\Users\\retse\\repos\\hera-data\\kernels\\spk\\HERA_didymoon_ECP_v01.bsp"
+    ECP_hera = "C:\\Users\\retse\\repos\\hera-data\\kernels\\spk\\HERA_sc_ECP_v01.bsp"
+    barycenter_spk = "C:\\Users\\retse\\repos\\hera-data\\kernels\\spk\\didymos_hor_000101_500101_v01.bsp"
+    furnsh("C:\\Users\\retse\\repos\\hera-data\\kernels\\fk\\hera_v07.tf")
+    furnsh("C:\\Users\\retse\\repos\\hera-data\\kernels\\fk\\hera_ops_v02.tf")
+    furnsh(leap_kernel)
     furnsh(planets_kernel)
     furnsh(didymos_kernel)
     furnsh(dimorphos_kernel)
@@ -42,6 +39,10 @@ function load_hera_spice_kernels()
     furnsh(bodies_pck)
     furnsh(hera_sclk)
     furnsh(hera_instruments)
+    furnsh(ECP_didymos)
+    furnsh(ECP_dimorpos)
+    furnsh(ECP_hera)
+    furnsh(barycenter_spk)
 end
 
 
