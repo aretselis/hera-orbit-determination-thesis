5f079fa518e494189b0c4b9d94437bc68ff25059
retse <anast.rets@hotmail.com>
2022-04-08 12:06:20 +0300

    Use semiparameter in (OE) -> (r,v)

--- Code/orbital_utilities.jl
+++ Code/orbital_utilities.jl
@@ -69,38 +69,22 @@ function orbital_elements_to_cartesian(a, e, i, Omega, omega, M, mu)
             ni = M
         end
         # In this case, p=a
-        r_pqw = [(a*cosd(ni))/(1+e*cosd(ni)); (a*sind(ni))/(1+e*cosd(ni)); 0.0]
-        v_pqw = [-sqrt(mu/a)*sind(ni); sqrt(mu/a)*(e+cosd(ni)); 0.0]
-        transformation = [cosd(Omega)*cosd(omega)-sind(Omega)*sind(omega)*cosd(i) -cosd(Omega)*sind(omega)-sind(Omega)*cosd(omega)*cosd(i) sind(Omega)*sind(i);
+        p = a
+    else
+        if i < 10^-5 
+            # Elliptical equatorial orbit
+            Omega = 0.0
+        end
+        ni = M
+        p = a*(1-e^2)
+    end
+    r_pqw = [(p*cosd(ni))/(1+e*cosd(ni)); (p*sind(ni))/(1+e*cosd(ni)); 0.0]
+    v_pqw = [-sqrt(mu/p)*sind(ni); sqrt(mu/p)*(e+cosd(ni)); 0.0]
+    transformation = [cosd(Omega)*cosd(omega)-sind(Omega)*sind(omega)*cosd(i) -cosd(Omega)*sind(omega)-sind(Omega)*cosd(omega)*cosd(i) sind(Omega)*sind(i);
                           sind(Omega)*cosd(omega)+cosd(Omega)*sind(omega)*cosd(i) -sind(Omega)*sind(omega)+cosd(Omega)*cosd(omega)*cosd(i) -cosd(Omega)*sind(i);
                           sind(omega)*sind(i) cosd(omega)*sind(i) cosd(i)]
-        r_vector = transformation*r_pqw
-        v_vector = transformation*v_pqw
-    else
-        # Convert M to radians
-        M = deg2rad(M)
-        # Compute E using Newton-Raphson
-        E = eccentric_anomaly_calculator(M, e)
-        # Compute x, xdot, y, ydot on the orbital plane
-        x = a * (cos(E) - e)
-        y = a * sqrt(1 - e^2) * sin(E)
-        r = sqrt(x^2 + y^2)
-        n = sqrt(mu / (a^3))  # Mean motion
-        x_dot = -(n * a^2 / r) * sin(E)
-        y_dot = (n * a^2 / r) * sqrt(1 - e^2) * cos(E)
-        # Rotation Matrices definition
-        Omega = deg2rad(Omega)
-        omega = deg2rad(omega)
-        i = deg2rad(i)
-        P1 = [cos(omega) -sin(omega) 0;sin(omega) cos(omega) 0;0 0 1]
-        P2 = [1 0 0;0 cos(i) sin(i);0 sin(i) cos(i)]
-        P3 = [cos(Omega) -sin(Omega) 0;sin(Omega) cos(Omega) 0;0 0 1]
-        # Compute cartesian coordinates
-        x_y_vector = [x, y, 0]
-        x_y_dot_vector = [x_dot, y_dot, 0]
-        r_vector = *(*(*(P3, P2), P1), x_y_vector)
-        v_vector = *(*(*(P3, P2), P1), x_y_dot_vector)
-    end
+    r_vector = transformation*r_pqw
+    v_vector = transformation*v_pqw
     return r_vector, v_vector
 end 
 
