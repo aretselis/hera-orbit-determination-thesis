e4ad5508b90beaee6ed328a39dc387351270252e
retse <anast.rets@hotmail.com>
2022-07-11 00:03:07 +0300

    Random initial vector generation and 3D plot fixes

--- Code/camera_spice_experiment.jl
+++ Code/camera_spice_experiment.jl
@@ -10,12 +10,12 @@ include("plotting.jl")
 
 function main()
     # Dimorphos orbit
-    global a_dimorphos = 1190.63 # Semi-major axis
-    global e_dimorphos = 0.0000062 #0.00000098 # Eccentricity
-    global i_dimorphos = 0.0042 #0.035 # Inclination
-    global Omega_dimorphos = 0.0 #0.0 # Longitude of the ascending node
-    global omega_dimorphos = 0.0 #0.0 # Argument of periapsis
-    global M_dimorphos = 278.96 #172.18 # True anomaly 
+    global a_dimorphos = rand(Uniform(1160, 1220)) # Semi-major axis
+    global e_dimorphos = rand(Uniform(0.0000001, 0.00001)) # Eccentricity
+    global i_dimorphos = rand(Uniform(0.00000001, 0.00001)) # Inclination
+    global Omega_dimorphos = 0.0 # Longitude of the ascending node
+    global omega_dimorphos = 0.0 # Argument of periapsis
+    global M_dimorphos = rand(Uniform(0.0, 359.99)) # True anomaly 
 
     # Determine what kind of orbit we are dealing with
     global dimorphos_orbit_type = type_of_orbit(e_dimorphos, i_dimorphos)
@@ -46,9 +46,6 @@ function main()
     vz_dimorphos = vz_dimorphos[index_vector]
     t_vector = t_vector[index_vector]
     dimorphos_coordinates = hcat(x_dimorphos/1000, y_dimorphos/1000, z_dimorphos/1000)
-
-    # Orbit start and end time (for SPICE)
-    spice_end_time = spice_start_time + end_time
 
     # Initialize position arrays (x, y, z)
     didymos_coordinates = zeros(Float64, total_photos, 3)
@@ -120,8 +117,8 @@ function main()
     pgfplotsx()   
     x_didymos, y_didymos, z_didymos = didymos_coordinates[:, 1], didymos_coordinates[:, 2], didymos_coordinates[:, 3]
     x_dimorphos, y_dimorphos, z_dimorphos = dimorphos_coordinates[:, 1], dimorphos_coordinates[:, 2], dimorphos_coordinates[:, 3]
-    plt_3d = scatter3d(x_didymos, y_didymos, z_didymos, color = "blue", xlabel="x [km]", ylabel="y [km]", zlabel = "z [km]", label="Didymos", markersize = 1)
-    scatter3d!(plt_3d, x_dimorphos, y_dimorphos, z_dimorphos, color = "orange", markersize = 1, label = "Dimorphos (RK4)")
+    plt_3d = scatter3d(x_didymos, y_didymos, z_didymos, color = "blue", xlabel="x [km]", ylabel="y [km]", zlabel = "z [km]", label="Didymos", markersize = 1.5)
+    scatter3d!(plt_3d, x_dimorphos, y_dimorphos, z_dimorphos, color = "orange", markersize = 1.5, label = "Dimorphos (RK4)")
 
     # Compute and plot camera boundary lines
     sensor_boundary_points = zeros(Float64, 4, 3)
@@ -135,7 +132,7 @@ function main()
         x_boundaries[i] = focal_length*x_line[length(x_line)]/z_line[length(z_line)]
         y_boundaries[i] = focal_length*y_line[length(y_line)]/z_line[length(z_line)]
         camera_label = "Camera boundary line " * string(i)
-        plot3d!(plt_3d, x_line, y_line, z_line, color="green", label=camera_label) 
+        #plot3d!(plt_3d, x_line, y_line, z_line, color="green", label=camera_label) 
     end
 
     # Compute coordinates in pixels
@@ -167,17 +164,16 @@ function main()
 
     usable_photos = length(collect(skipmissing(x_pixel_dimorphos)))
     println("Usable photos: " *string(usable_photos))
-    
+
     # Perform orbit determination
     if dimorphos_orbit_type == "Normal"
-        minimum_error = residuals([a_dimorphos e_dimorphos i_dimorphos Omega_dimorphos omega_dimorphos M_dimorphos])
+        minimum_error = residuals([a_dimorphos e_dimorphos i_dimorphos Omega_dimorphos omega_dimorphos M_dimorphos mu_system])
         println("Minimum error is " *string(minimum_error)* " pixels")
         # Full OD problem, 6 orbital elements
-        bounds = [1190.0-30 0.0001 0.0 0.0 0.0 0.0; 
-                  1190.0+30 0.9 90.0 359.99 359.99 359.99]
+        bounds = [1190.0-30 0.0001 0.0 0.0 0.0 0.0 0.8*mu_system_original; 
+                  1190.0+30 0.9 90.0 359.99 359.99 359.99 1.2*mu_system_original]
         # Minimize square mean error to find best orbital elements
         result = optimize(residuals, bounds, ECA(information = Information(f_optimum = minimum_error), options = Options(debug=true, iterations=100, store_convergence = true)))
-        #result = optimize(residuals, bounds, PSO(information = Information(f_optimum = minimum_error),options = Options(debug=true, iterations=100, store_convergence = true)))
         # Extract final guess from optimization
         a_final = minimizer(result)[1]
         e_final = minimizer(result)[2]
@@ -185,28 +181,28 @@ function main()
         Omega_final = minimizer(result)[4]
         omega_final = minimizer(result)[5]
         M_final = minimizer(result)[6]
+        mu_final = minimizer(result)[7]
     elseif dimorphos_orbit_type == "EEO"
-        minimum_error = residuals([a_dimorphos e_dimorphos i_dimorphos omega_dimorphos M_dimorphos])
+        minimum_error = residuals([a_dimorphos e_dimorphos omega_dimorphos M_dimorphos mu_system])
         println("Minimum error is " *string(minimum_error)* " pixels")
         # Elliptical Equatorial orbit, OD with 5 orbital elements
         # Bounds for optimization
-        bounds = [1190.0-30 0.00000001 0.001 0.0 0.0;
-                  1190.0+30 0.9 0.1 359.9 359.99]
+        bounds = [1190.0-30 0.00000001 0.0 0.0 0.8*mu_system_original;
+                  1190.0+30 0.9 359.9 359.99 1.2*mu_system_original]
         result = optimize(residuals, bounds, ECA(information = Information(f_optimum = minimum_error), options = Options(debug=true, iterations=100, store_convergence = true)))
         # Extract final guess from optimization
         a_final = minimizer(result)[1]
         e_final = minimizer(result)[2]
-        i_final = minimizer(result)[3]
+        i_final = i_dimorphos
         Omega_final = Omega_dimorphos
-        omega_final = minimizer(result)[4]
-        M_final = minimizer(result)[5]
+        omega_final = minimizer(result)[3]
+        M_final = minimizer(result)[4]
+        mu_final = minimizer(result)[5]
     elseif dimorphos_orbit_type == "CIO"
-        minimum_error = residuals([a_dimorphos e_dimorphos i_dimorphos Omega_dimorphos M_dimorphos])
+        minimum_error = residuals([a_dimorphos e_dimorphos i_dimorphos Omega_dimorphos M_dimorphos mu_system])
         println("Minimum error is " *string(minimum_error)* " pixels")
         # Circular inclined orbit, OD with 5 orbital elements
         # Bounds for optimization
-        bounds = [1190.0-30 0.00000001 0.0 0.0 0.0;
-                  1190.0+30 0.00001 90.0 359.99 359.99]
         result = optimize(residuals, bounds, ECA(information = Information(f_optimum = minimum_error), options = Options(debug=true, iterations=100, store_convergence = true)))
         # Extract final guess from optimization
         a_final = minimizer(result)[1]
@@ -215,21 +211,23 @@ function main()
         Omega_final = minimizer(result)[4]
         omega_final = omega_dimorphos
         M_final = minimizer(result)[5]
+        mu_final = minimizer(result)[6]
     elseif dimorphos_orbit_type == "CEO"
-        minimum_error = residuals([a_dimorphos e_dimorphos i_dimorphos M_dimorphos])
+        minimum_error = residuals([a_dimorphos e_dimorphos M_dimorphos mu_system])
         println("Minimum error is " *string(minimum_error)* " pixels")
         # Circular equatorial orbit, OD with 4 orbital elements
         # Bounds for optimization
-        bounds = [1190.0-30 0.00000001 0.001 0.0;
-                  1190.0+30 0.00001 0.1 359.99]
+        bounds = [1190.0-30 0.0000001 0.0 0.8*mu_system_original;
+                  1190.0+30 0.00001 359.99 1.2*mu_system_original]
         result = optimize(residuals, bounds, ECA(information = Information(f_optimum = minimum_error), options = Options(debug=true, iterations=100, store_convergence = true)))
         # Extract final guess from optimization
         a_final = minimizer(result)[1]
         e_final = minimizer(result)[2]
-        i_final = minimizer(result)[3]
+        i_final = i_dimorphos
         Omega_final = Omega_dimorphos
         omega_final = omega_dimorphos
-        M_final = minimizer(result)[4]
+        M_final = minimizer(result)[3]
+        mu_final = minimizer(result)[4]
     end
 
     # Plot performance
@@ -244,33 +242,52 @@ function main()
 
     # Compute percentage error of the predicted orbit relative to the initial orbit 
     initial_elements = [a_dimorphos, e_dimorphos, i_dimorphos, Omega_dimorphos, omega_dimorphos, M_dimorphos]
-    final_elements = [a_final, e_final, i_final, Omega_final, omega_final, M_final]
+    final_elements = [a_final, e_final, i_final, Omega_final, omega_final, M_final, mu_final]
+    println(initial_elements)
     println(final_elements)
-    compute_percentage_error(initial_elements, final_elements)
     compute_mean_absolute_percentage_error(initial_elements, final_elements)
+    GM_percentage_error = 100*(mu_system_original-mu_final)/mu_system_original 
+    println("Error on GM_system is " * string(GM_percentage_error) * " %")
 
     # Print number of usable photos
     println("Total photos: " * string(total_photos))
     println("Usable photos: " * string(usable_photos))
     # Compute pixel and 3D points from dimorphos as a result of the optimization
-    x_pixel_fit_dimorphos, y_pixel_fit_dimorphos = propagate_and_compute_dimorphos_pixel_points(a_final, e_final, i_final, Omega_final, omega_final, M_final, start_time, end_time, step_size, spice_start_time)
-    dimorphos_fit_coordinates = propagate_and_compute_dimorphos_3D_points(a_final, e_final, i_final, Omega_final, omega_final, M_final, start_time, end_time, step_size, spice_start_time)
-    x_fit_dimorphos = dimorphos_fit_coordinates[:, 1]
-    y_fit_dimorphos = dimorphos_fit_coordinates[:, 2]
-    z_fit_dimorphos = dimorphos_fit_coordinates[:, 3]
-    #scatter3d!(x_fit_dimorphos, y_fit_dimorphos, z_fit_dimorphos, color = "purple", markersize = 1, label = "Dimorphos (Final guess)")
+    x_pixel_fit_dimorphos, y_pixel_fit_dimorphos = propagate_and_compute_dimorphos_pixel_points(a_final, e_final, i_final, Omega_final, omega_final, M_final, start_time, end_time, step_size, spice_start_time, mu_final)
+    dimorphos_fit_coordinates = propagate_and_compute_dimorphos_3D_points(a_final, e_final, i_final, Omega_final, omega_final, M_final, start_time, end_time, step_size, spice_start_time, mu_final)
+    
+    # Don't plot fit 3D points which don't exist in photos
+    x_fit_dimorphos = Array{Union{Missing, Float64}, 1}(missing, total_photos)
+    z_fit_dimorphos = Array{Union{Missing, Float64}, 1}(missing, total_photos)
+    y_fit_dimorphos = Array{Union{Missing, Float64}, 1}(missing, total_photos)
+    for i=1:size(dimorphos_coordinates)[1]
+        x_fit_dimorphos[i] = dimorphos_fit_coordinates[i, 1]
+        y_fit_dimorphos[i] = dimorphos_fit_coordinates[i, 2]
+        z_fit_dimorphos[i] = dimorphos_fit_coordinates[i, 3]
+    end
+    for i in 1:length(x_pixel_dimorphos)
+        if ismissing(x_pixel_fit_dimorphos[i]) || ismissing(y_pixel_fit_dimorphos[i])
+            x_fit_dimorphos[i] = missing
+            y_fit_dimorphos[i] = missing
+            z_fit_dimorphos[i] = missing
+        end
+    end 
+    
+    scatter3d!(plt_3d, x_fit_dimorphos, y_fit_dimorphos, z_fit_dimorphos, color = "purple", markersize = 1.5, label = "Dimorphos (Final guess)")
 
     # Plot 2D image simulation (camera plane)  
     plt_2d = scatter(didymos_pixel_coordinates[:, 1], didymos_pixel_coordinates[:, 2], label= "Didymos")
     scatter!(dimorphos_pixel_coordinates[:, 1], dimorphos_pixel_coordinates[:, 2], label = "Dimorphos")
     scatter!(x_boundaries, y_boundaries, label = "Camera boundaries")
+    savefig(plt_3d, ".\\Results\\3d_plot.pdf")
 
     # Plot image using pixels 
-    plt_pixel = scatter(x_pixel_didymos, y_pixel_didymos, label = "Didymos")
+    plt_pixel = scatter(x_pixel_didymos, y_pixel_didymos, label = "Didymos", widen=true, formatter=:plain, legend = :topright, xlabel = "X Pixel Coordinate", ylabel = "Y Pixel Coordinate")
     scatter!(x_pixel_dimorphos, y_pixel_dimorphos, label = "Dimorphos")
-    #scatter!(x_pixel_fit_dimorphos, y_pixel_fit_dimorphos, label = "Dimorphos best fit")
+    scatter!(x_pixel_fit_dimorphos, y_pixel_fit_dimorphos, label = "Dimorphos best fit")
     scatter!(x_pixel_boundaries, y_pixel_boundaries, label= "Pixel boundaries")
-    display(plt_performance)
+    savefig(plt_pixel, ".\\Results\\pixel_error_example.pdf")
+    display(plt_pixel)
 end
 
 load_hera_spice_kernels()
@@ -282,8 +299,9 @@ mass_error_distribution = Uniform(-mass_error, mass_error)
 mass_didymos = 5.32*10^11 + rand(mass_error_distribution)*5.32*10^11
 global radius_didymos = 390 # [m]
 global J2_didymos = 0.011432722
-global mass_dimorphos = 4.94*10^9  + rand(mass_error_distribution)*4.94*10^9
+global mass_dimorphos = 4.94*10^9 + rand(mass_error_distribution)*4.94*10^9
 global mu_system = G*(mass_didymos+mass_dimorphos)
+global mu_system_original = mu_system
 global c = 3.0*10^8
 global enable_perturbation = true
 
@@ -296,8 +314,7 @@ global total_photos = photos_per_orbit * number_of_orbits
 global start_time = 0.0
 global end_time = 300*hour
 global step_size = floor((end_time-start_time)/total_photos)
-global spice_start_time = utc2et("2027-02-28T08:14:58")
-global flux = 1358
+global spice_start_time = utc2et("2027-01-28T08:14:58")
 
 # Errors definition
 # Centroid pixel error
@@ -317,13 +334,14 @@ global pointing_error_distribution = Normal(0.0, pointing_error)
 global random_error = rand(pointing_error_distribution, total_photos)
 global random_axis = rand(1:2, total_photos)
 # Randomly dropped images
-number_of_dropped_images = Int64(round(0.1*total_photos))
-dropped_image_distribution = Uniform(0, total_photos)
+number_of_dropped_images = Int64(round(0.04*total_photos))
+dropped_image_distribution = Uniform(1, total_photos)
 global dropped_images_index = zeros(Int64, number_of_dropped_images) 
 for p=1:number_of_dropped_images
     dropped_images_index[p] = Int64(round(rand(dropped_image_distribution)))
 end
 
+# Run experiment
 main()
 
 # Free used kernels
