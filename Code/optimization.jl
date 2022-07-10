72301c2713a37a3a56238f3aa9b50743e0831690
retse <anast.rets@hotmail.com>
2022-07-10 23:59:18 +0300

    Fit gravitational parameter, deal with empty vectors and add wobble cost function

--- Code/optimization.jl
+++ Code/optimization.jl
@@ -7,13 +7,15 @@ function residuals(orbit)
         Omega = orbit[4]
         omega = orbit[5]
         M = orbit[6]
+        mu = orbit[7]
     elseif dimorphos_orbit_type == "EEO"
         a = orbit[1]
         e = orbit[2]
-        i = orbit[3]
+        i = i_dimorphos
         Omega = Omega_dimorphos
-        omega = orbit[4]
-        M = orbit[5]
+        omega = orbit[3]
+        M = orbit[4]
+        mu = orbit[5]
     elseif dimorphos_orbit_type == "CIO"
         a = orbit[1]
         e = orbit[2]
@@ -21,21 +23,31 @@ function residuals(orbit)
         Omega = orbit[4]
         omega = omega_dimorphos
         M = orbit[5]
+        mu = orbit[6]
     else
         a = orbit[1]
         e = orbit[2]
-        i = orbit[3]
+        i = i_dimorphos
         Omega = Omega_dimorphos
         omega = omega_dimorphos
-        M = orbit[4]
+        M = orbit[3]
+        mu = orbit[4]
     end
     # Compute dimorphos pixel points for given orbit and compute xhi2
     observed_x, observed_y = x_pixel_dimorphos, y_pixel_dimorphos
-    predicted_x, predicted_y = propagate_and_compute_dimorphos_pixel_points(a, e, i, Omega, omega, M, start_time, end_time, step_size, spice_start_time)
+    predicted_x, predicted_y = propagate_and_compute_dimorphos_pixel_points(a, e, i, Omega, omega, M, start_time, end_time, step_size, spice_start_time, mu)
     xhi2 = sum_of_squared_residuals(observed_x, observed_y, predicted_x, predicted_y)
     return Float64(xhi2)
 end
 
+function wobble(masses)
+    current_dimorphos_mass = masses[1]
+    # Compute dimorphos pixel points for given orbit and compute xhi2
+    observed_x, observed_y = x_pixel_didymos, y_pixel_didymos
+    predicted_x, predicted_y = propagate_and_dimorphos(current_dimorphos_mass)
+    xhi2 = sum_of_squared_residuals(observed_x, observed_y, predicted_x, predicted_y)
+    return Float64(xhi2)
+end
 
 function sum_of_squared_residuals(observed_x, observed_y, predicted_x, predicted_y)
     x_residuals = observed_x - predicted_x
@@ -43,7 +55,11 @@ function sum_of_squared_residuals(observed_x, observed_y, predicted_x, predicted
     # Clean up missing values
     x_residuals = collect(skipmissing(x_residuals))
     y_residuals = collect(skipmissing(y_residuals))
-    squared_sum = sum(x_residuals.^2) + sum(y_residuals.^2)
+    if isempty(x_residuals) || isempty(y_residuals) 
+        return 10^8
+    else
+        squared_sum = sum(x_residuals.^2) + sum(y_residuals.^2)
+    end
     return squared_sum
 end
 
